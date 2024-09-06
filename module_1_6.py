dc11e464270712c80958360da0a0512e5f9b700e
DjonHd <djonohara@gmail.com>
2024-09-06 11:33:04 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,26 @@
+my_dict = {'Katy':2004,'Elis':2007,'Mary':2009}
+print(my_dict)
+print(my_dict['Katy'])
+print(my_dict.get('Viki'))
+a = my_dict.pop('Mary')
+print(a)
+my_dict['Viki']=2010
+my_dict['Djon']=1981
+print(my_dict)
+my_set ={1,3,'название',3,85}
+print(my_set)
+print(my_set.pop())
+list_ =[2,'новое']
+list_ =set(list_)
+print(list_)
+print(my_set.union(list_))
+
+
+
+
+
+
+
+
+
+
